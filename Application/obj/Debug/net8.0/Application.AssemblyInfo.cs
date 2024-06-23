92b8dc1affd585c178e2100c595a9226cba57f38
Gabe Arnold <gabearnold@Gabes-MacBook-Pro.local>
2024-06-23 11:10:54 -0400

    update Dockerfile

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24889a9304fc79e1c6970c59a7ed14852fdf9422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+758a2203a922af95d6aa534e97794d55bfa050e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
